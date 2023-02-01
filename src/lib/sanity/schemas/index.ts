7adf805411c5ea8311c5553bd5a92a317cd354e1
TheBinaryGuy <hello@thebinary.dev>
2023-02-01 18:07:15 +0530

    feat: added site to schema list

--- src/lib/sanity/schemas/index.ts
+++ src/lib/sanity/schemas/index.ts
@@ -2,6 +2,7 @@ import author from '@/lib/sanity/schemas/author';
 import blockContent from '@/lib/sanity/schemas/blockContent';
 import category from '@/lib/sanity/schemas/category';
 import post from '@/lib/sanity/schemas/post';
+import site from '@/lib/sanity/schemas/site';
 import tag from '@/lib/sanity/schemas/tag';
 
-export const schemaTypes = [post, author, tag, category, blockContent];
+export const schemaTypes = [post, author, tag, category, blockContent, site];
