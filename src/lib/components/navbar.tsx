c1f75734352ae0c999da782c1e023e1be9c9ad5a
TheBinaryGuy <hello@thebinary.dev>
2023-02-09 06:39:03 +0530

    style: improved home page a bit

--- src/lib/components/navbar.tsx
+++ src/lib/components/navbar.tsx
@@ -1,25 +1,20 @@
+import { Logo } from '@/lib/components/logo';
 import { clientRoutes } from '@/lib/data/routes';
 import Link from 'next/link';
 
 export const Navbar = () => (
     <nav className='h-20 w-full shrink-0 bg-white shadow shadow-slate-200'>
         <div className='container mx-auto flex h-full items-center justify-between p-4'>
-            <Link href='/'>
-                <h1 className='text-2xl'>Hushify</h1>
+            <Link href='/' className='flex items-center gap-1'>
+                <Logo className='w-12' />
+                <span className='text-4xl'>Hushify</span>
             </Link>
             <ul className='flex items-center justify-center gap-4 md:gap-8'>
                 <li>
                     <Link
                         href={clientRoutes.blog.index}
-                        className='font-mono hover:underline'>
-                        /blog
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        href={clientRoutes.identity.login}
-                        className='font-mono hover:underline'>
-                        /login
+                        className='hover:underline'>
+                        Blog
                     </Link>
                 </li>
             </ul>
