ecde0f0e5979938f7dc433be22ba4e71353359f0
TheBinaryGuy <hello@thebinary.dev>
2023-03-02 19:27:25 +0530

    perf: removed zxcvbn dictionaries

--- src/lib/utils/zxcvbn.ts
+++ src/lib/utils/zxcvbn.ts
@@ -1,17 +1,1 @@
-import { zxcvbnOptions } from '@zxcvbn-ts/core';
-import { OptionsType } from '@zxcvbn-ts/core/dist/types';
-import zxcvbnCommonPackage from '@zxcvbn-ts/language-common';
-import zxcvbnEnPackage from '@zxcvbn-ts/language-en';
-
-const options: OptionsType = {
-    translations: zxcvbnEnPackage.translations,
-    graphs: zxcvbnCommonPackage.adjacencyGraphs,
-    dictionary: {
-        ...zxcvbnCommonPackage.dictionary,
-        ...zxcvbnEnPackage.dictionary,
-    },
-};
-
-zxcvbnOptions.setOptions(options);
-
 export { zxcvbn } from '@zxcvbn-ts/core';
