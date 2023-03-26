baad47c2a64ff0ac33f1bd5fbfb311b0dc9c8de1
TheBinaryGuy <hello@thebinary.dev>
2023-03-27 00:16:14 +0530

    error boundary changes

--- src/app/(drive)/error.tsx
+++ src/app/(drive)/error.tsx
@@ -1,24 +1,14 @@
 'use client';
 
-import { useEffect } from 'react';
 import { ServerCrash } from 'lucide-react';
 
-import { cn } from '@/lib/utils/cn';
-
-export default function Error({ error, reset }: { error: Error; reset: () => void }) {
-    useEffect(() => {
-        console.error(error);
-    }, [error]);
-
+export default function Error({ error: _, reset }: { error: Error; reset: () => void }) {
     return (
         <div className='flex h-full w-full flex-col items-center justify-center gap-4 text-center'>
             <h2 className='text-2xl font-medium'>Something went wrong!</h2>
             <button
                 onClick={reset}
-                className={cn(
-                    'flex cursor-pointer items-center justify-center gap-1 rounded-lg px-2.5 py-1.5 font-medium',
-                    'bg-brand-600 text-white focus-visible:ring-brand-600/75'
-                )}>
+                className='flex cursor-pointer items-center justify-center gap-1 rounded-lg bg-brand-600 px-2.5 py-1.5 font-medium text-white focus-visible:ring-brand-600/75'>
                 <span>Try again</span>
                 <ServerCrash className='h-4 w-4' />
             </button>
