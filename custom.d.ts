c90f60b75973af51674ed8b276c1d625bca4ec5c
TheBinaryGuy <hello@thebinary.dev>
2023-04-18 00:12:06 +0530

    removed unnecessary types

--- custom.d.ts
+++ custom.d.ts
@@ -1,13 +1,2 @@
 declare module 'client-only';
 declare module 'server-only';
-
-declare type MDXMeta = {
-    title: string;
-    author: string;
-    publishedAt: string;
-    excerpt: string;
-    category: string;
-    tags: string[];
-};
-
-declare module 'xlsx-viewer';
