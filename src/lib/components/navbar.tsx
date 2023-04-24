5812e5a15eb6df2f47c90461f93450da4d621d2a
TheBinaryGuy <hello@thebinary.dev>
2023-04-24 22:29:55 +0530

    fixed modal issue and added a hamburger menu

--- src/lib/components/navbar.tsx
+++ src/lib/components/navbar.tsx
@@ -1,65 +1,251 @@
+'use client';
+
+import { Fragment } from 'react';
 import Link from 'next/link';
+import { Popover, Transition } from '@headlessui/react';
+import clsx from 'clsx';
 
 import { Logo } from '@/lib/components/logo';
 import { clientRoutes } from '@/lib/data/routes';
+import { useAuthStore } from '@/lib/stores/auth-store';
 import { cn } from '@/lib/utils/cn';
 
-export function Navbar() {
+function MobileNavigation() {
+    const status = useAuthStore(state => state.status);
+    const logout = useAuthStore(state => state.logout);
+
     return (
-        <nav className='fixed z-10 h-16 w-full shrink-0 bg-white/75 shadow backdrop-blur'>
+        <Popover>
+            {({ open, close }) => (
+                <>
+                    <Popover.Button className='relative z-10 flex h-8 w-8 items-center justify-center [&:not(:focus-visible)]:focus:outline-none'>
+                        <span className='sr-only'>Toggle Navigation</span>
+                        <svg
+                            aria-hidden='true'
+                            className='h-3.5 w-3.5 overflow-visible stroke-slate-700'
+                            fill='none'
+                            strokeWidth={2}
+                            strokeLinecap='round'>
+                            <path
+                                d='M0 1H14M0 7H14M0 13H14'
+                                className={clsx('origin-center transition', {
+                                    'scale-90 opacity-0': open,
+                                })}
+                            />
+                            <path
+                                d='M2 2L12 12M12 2L2 12'
+                                className={clsx('origin-center transition', {
+                                    'scale-90 opacity-0': !open,
+                                })}
+                            />
+                        </svg>
+                    </Popover.Button>
+                    <Transition.Root>
+                        <Transition.Child
+                            as={Fragment}
+                            enter='duration-150 ease-out'
+                            enterFrom='opacity-0'
+                            enterTo='opacity-100'
+                            leave='duration-150 ease-in'
+                            leaveFrom='opacity-100'
+                            leaveTo='opacity-0'>
+                            <Popover.Overlay className='fixed inset-0 bg-slate-300/50' />
+                        </Transition.Child>
+                        <Transition.Child
+                            as={Fragment}
+                            enter='duration-150 ease-out'
+                            enterFrom='opacity-0 scale-95'
+                            enterTo='opacity-100 scale-100'
+                            leave='duration-100 ease-in'
+                            leaveFrom='opacity-100 scale-100'
+                            leaveTo='opacity-0 scale-95'>
+                            <Popover.Panel
+                                as='ul'
+                                className='absolute inset-x-0 top-full mx-4 mt-4 origin-top space-y-4 rounded-2xl bg-white p-6 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5'>
+                                <li className='block w-full'>
+                                    <Link
+                                        onClick={close}
+                                        href={clientRoutes.blog.index}
+                                        className='text-lg duration-200 ease-out hover:text-brand-600'>
+                                        Blog
+                                    </Link>
+                                </li>
+                                <li className='block w-full'>
+                                    <Link
+                                        onClick={close}
+                                        href={clientRoutes.security}
+                                        className='text-lg duration-200 ease-out hover:text-brand-600'>
+                                        Security
+                                    </Link>
+                                </li>
+                                <li className='block w-full'>
+                                    <Link
+                                        onClick={close}
+                                        href={clientRoutes.about}
+                                        className='text-lg duration-200 ease-out hover:text-brand-600'>
+                                        About
+                                    </Link>
+                                </li>
+                                <li className='block w-full'>
+                                    <Link
+                                        onClick={close}
+                                        href={clientRoutes.legal}
+                                        className='text-lg duration-200 ease-out hover:text-brand-600'>
+                                        Legal
+                                    </Link>
+                                </li>
+                                {status === 'loading' && (
+                                    <li className='block w-full'>
+                                        <span className='text-lg duration-200 ease-out hover:text-brand-600'>
+                                            Loading
+                                        </span>
+                                    </li>
+                                )}
+                                {status === 'unauthenticated' && (
+                                    <>
+                                        <li className='block w-full'>
+                                            <Link
+                                                onClick={close}
+                                                href={clientRoutes.identity.login}
+                                                className='text-lg duration-200 ease-out hover:text-brand-600'>
+                                                Login
+                                            </Link>
+                                        </li>
+                                        <li className='block w-full'>
+                                            <Link
+                                                onClick={close}
+                                                href={clientRoutes.identity.register}
+                                                className='text-lg duration-200 ease-out hover:text-brand-600'>
+                                                Get Started
+                                            </Link>
+                                        </li>
+                                    </>
+                                )}
+                                {status === 'authenticated' && (
+                                    <>
+                                        <li className='block w-full'>
+                                            <Link
+                                                onClick={close}
+                                                href={clientRoutes.drive}
+                                                className='text-lg duration-200 ease-out hover:text-brand-600'>
+                                                My Drive
+                                            </Link>
+                                        </li>
+                                        <li className='block w-full'>
+                                            <button
+                                                onClick={async () => {
+                                                    await logout();
+                                                    close();
+                                                }}
+                                                className='text-lg duration-200 ease-out hover:text-brand-600'>
+                                                Logout
+                                            </button>
+                                        </li>
+                                    </>
+                                )}
+                            </Popover.Panel>
+                        </Transition.Child>
+                    </Transition.Root>
+                </>
+            )}
+        </Popover>
+    );
+}
+
+export function Navbar() {
+    const status = useAuthStore(state => state.status);
+    const logout = useAuthStore(state => state.logout);
+
+    return (
+        <header className='fixed z-10 h-16 w-full shrink-0 bg-white/75 shadow backdrop-blur'>
             <div className='container mx-auto flex h-full items-center justify-between p-4'>
                 <Link href='/' className='flex items-center gap-1.5 lg:gap-2'>
                     <Logo className='w-6' />
                     <span className='text-2xl font-medium'>Hushify</span>
                 </Link>
                 <ul className='flex items-center justify-center gap-4 md:gap-6'>
-                    <li>
+                    <li className='hidden lg:block'>
                         <Link
                             href={clientRoutes.blog.index}
                             className='text-lg duration-200 ease-out hover:text-brand-600'>
                             Blog
                         </Link>
                     </li>
-                    <li>
+                    <li className='hidden lg:block'>
                         <Link
                             href={clientRoutes.security}
                             className='text-lg duration-200 ease-out hover:text-brand-600'>
                             Security
                         </Link>
                     </li>
-                    <li>
+                    <li className='hidden lg:block'>
                         <Link
                             href={clientRoutes.about}
                             className='text-lg duration-200 ease-out hover:text-brand-600'>
                             About
                         </Link>
                     </li>
-                    <li>
+                    <li className='hidden lg:block'>
                         <Link
                             href={clientRoutes.legal}
                             className='text-lg duration-200 ease-out hover:text-brand-600'>
                             Legal
                         </Link>
                     </li>
-                    <li>
-                        <Link
-                            href={clientRoutes.identity.login}
-                            className='text-lg duration-200 ease-out hover:text-brand-600'>
-                            Login
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href={clientRoutes.identity.register}
-                            className={cn(
-                                'flex items-center justify-center gap-2 rounded-lg px-4 py-1.5 font-medium',
-                                'bg-brand-600 text-white focus-visible:ring-brand-600/75'
-                            )}>
-                            Get Started
-                        </Link>
+                    {status === 'loading' && (
+                        <li>
+                            <span className='text-lg duration-200 ease-out hover:text-brand-600'>
+                                Loading
+                            </span>
+                        </li>
+                    )}
+                    {status === 'unauthenticated' && (
+                        <>
+                            <li className='hidden lg:block'>
+                                <Link
+                                    href={clientRoutes.identity.login}
+                                    className='text-lg duration-200 ease-out hover:text-brand-600'>
+                                    Login
+                                </Link>
+                            </li>
+                            <li className='hidden lg:block'>
+                                <Link
+                                    href={clientRoutes.identity.register}
+                                    className={cn(
+                                        'flex items-center justify-center gap-2 rounded-lg px-4 py-1.5 font-medium',
+                                        'bg-brand-600 text-white focus-visible:ring-brand-600/75'
+                                    )}>
+                                    Get Started
+                                </Link>
+                            </li>
+                        </>
+                    )}
+                    {status === 'authenticated' && (
+                        <>
+                            <li className='hidden lg:block'>
+                                <button
+                                    onClick={logout}
+                                    className='text-lg duration-200 ease-out hover:text-brand-600'>
+                                    Logout
+                                </button>
+                            </li>
+                            <li className='hidden lg:block'>
+                                <Link
+                                    href={clientRoutes.drive}
+                                    className={cn(
+                                        'flex items-center justify-center gap-2 rounded-lg px-4 py-1.5 font-medium',
+                                        'bg-brand-600 text-white focus-visible:ring-brand-600/75'
+                                    )}>
+                                    My Drive
+                                </Link>
+                            </li>
+                        </>
+                    )}
+                    <li className='lg:hidden'>
+                        <MobileNavigation />
                     </li>
                 </ul>
             </div>
-        </nav>
+        </header>
     );
 }
--- src/lib/components/scroll-area.tsx
+++ src/lib/components/scroll-area.tsx
@@ -20,12 +20,12 @@ export function ScrollArea({
             <Scrollbar
                 className='flex touch-none select-none bg-gray-200 p-0.5 transition-colors duration-150 ease-out hover:bg-gray-400 data-[orientation=horizontal]:h-2.5 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col'
                 orientation='vertical'>
-                <Thumb className="before:-trangray-x-1/2 before:-trangray-y-1/2 relative flex-1 rounded-[10px] bg-gray-500 before:absolute before:left-1/2 before:top-1/2 before:h-full before:min-h-[44px] before:w-full before:min-w-[44px] before:content-['']" />
+                <Thumb className="relative flex-1 rounded-[10px] bg-gray-500 before:absolute before:left-1/2 before:top-1/2 before:h-full before:min-h-[44px] before:w-full before:min-w-[44px] before:-translate-x-1/2 before:-translate-y-1/2 before:content-['']" />
             </Scrollbar>
             <Scrollbar
                 className='flex touch-none select-none bg-gray-200 p-0.5 transition-colors duration-150 ease-out hover:bg-gray-400 data-[orientation=horizontal]:h-2.5 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col'
                 orientation='horizontal'>
-                <Thumb className="before:-trangray-x-1/2 before:-trangray-y-1/2 relative flex-1 rounded-[10px] bg-gray-500 before:absolute before:left-1/2 before:top-1/2 before:h-full before:min-h-[44px] before:w-full before:min-w-[44px] before:content-['']" />
+                <Thumb className="relative flex-1 rounded-[10px] bg-gray-500 before:absolute before:left-1/2 before:top-1/2 before:h-full before:min-h-[44px] before:w-full before:min-w-[44px] before:-translate-x-1/2 before:-translate-y-1/2 before:content-['']" />
             </Scrollbar>
             <Corner className='bg-gray-500' />
         </Root>
