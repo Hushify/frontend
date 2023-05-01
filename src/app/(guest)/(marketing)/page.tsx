c806edb604a415b5d879088a6e8c6dcac2b53c02
TheBinaryGuy <hello@thebinary.dev>
2023-05-01 11:54:19 +0530

    fixed heading order on the home page

--- src/app/(guest)/(marketing)/page.tsx
+++ src/app/(guest)/(marketing)/page.tsx
@@ -102,9 +102,9 @@ export default function Home() {
             <div className='py-16 lg:py-24'>
                 <div className='mx-auto max-w-7xl px-6 lg:px-8'>
                     <div className='mx-auto max-w-2xl lg:text-center'>
-                        <h3 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+                        <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
                             Why Hushify?
-                        </h3>
+                        </h2>
                     </div>
                     <div className='mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl'>
                         <dl className='grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16'>
